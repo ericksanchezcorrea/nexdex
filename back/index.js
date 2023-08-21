0a0991c43beafceb14f7738b2ff989a26b6eee9b
erick <erick.sanchezcorrea@gmail.com>
2023-08-21 17:18:26 -0500

    se quitó '/' de la url

--- back/index.js
+++ back/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false})) 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://www.nexdext.com/' );
+    res.header('Access-Control-Allow-Origin', 'https://www.nexdext.com' );
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'POST');
