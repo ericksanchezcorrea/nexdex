72cd86f7244e72753ab9a9a9373b32a19560e79a
erick <erick.sanchezcorrea@gmail.com>
2023-09-18 00:57:16 -0500

    cambio de carpeta

---
+++ front/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "./category/**/*.{html,js}"], // Incluye la carpeta "category" y subcarpetas
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
